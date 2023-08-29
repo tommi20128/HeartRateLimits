dfb3c0f5a65e581fb242af97741a3d2a9d2024b9
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-08-29 21:02:22 +0300

    HeartRateLimits tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [hrLimits, setHrLimits] = useState('')
+
+  const calculateHrRate = (value) => {
+    const numericValue = parseFloat(value);
+    if (!isNaN(numericValue)) {
+      const lowerLimit = (220 - numericValue) * 0.65;
+      const upperLimit = (220 - numericValue) * 0.85;
+      setHrLimits(`${lowerLimit.toFixed(0)} - ${upperLimit.toFixed(0)}`);
+    } else {
+      setHrLimits('')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        placeholder='Put your age here'
+        value={age}
+        keyboardType='decimal-pad'
+        style={styles.field}
+        onChangeText= {text => {
+          setAge(text)
+          calculateHrRate(text)
+        }
+        }
+      />
+      <Text style={styles.field}>Hr limits</Text>
+      <Text style={styles.field}>{hrLimits}</Text>
     </View>
   );
 }
@@ -14,7 +40,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 15,
+    marginRight: 15,
   },
+  field: {
+    marginBottom: 10,
+  }
 });
